89fba12da9fb7d9c42571de114e7c8fad26553ed
rohitt0007 <rohittr0070@gmail.com>
2024-10-19 10:34:18 +0530

    - Accept two integers from user and print the sum // 	Ex - The sum of 45 & 12 = 57

--- 100.js
+++ 100.js
@@ -7,8 +7,20 @@
 // Print the messege along with values and sum
 // 	Ex - The sum of 45 & 12  = 57
 
-let num1 = 45;
-let num2 = 12;
+// let num1 = 45;
+// let num2 = 12;
 
-let sum = num1 + num2;
-console.log(`The sum of ${num1} & ${num2} = ${sum}`)
+// let sum = num1 + num2;
+// console.log(`The sum of ${num1} & ${num2} = ${sum}`)
+
+// ***************************************************************
+// ***************************************************************
+
+// - Accept two integers from user and print the sum
+// 	Ex - The sum of 45 & 12 = 57
+
+let a = +prompt("enter number");
+let b = +prompt("enter number");
+
+let sum = a + b;
+console.log(`The sum of ${a} & ${b} = ${sum}`)
